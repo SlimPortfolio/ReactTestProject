588f0153ee26461bc461a225608ed541c991663f
SlimPortfolio <103760212+SlimPortfolio@users.noreply.github.com>
2023-04-14 16:59:49 -0500

    added Events List title to Events.jsx page

--- client/src/Components/Events.jsx
+++ client/src/Components/Events.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 import TabBar from "./TabBar";
 import EventCard from "./EventCard";
+import { Container, Typography } from "@mui/material";
+
 
 function Events() {
     const data = [{"title":"Basketball Game","description":"Watch David take down the Gilstads"},{"title":"Sam Yang Birthday Party","description":"Dude's 12"}];
@@ -10,6 +12,12 @@ function Events() {
                 <header>
                     <TabBar tabValue = {1}/>
                 </header>
+            </Fragment>
+            <Fragment>
+                <Container maxWidth="md">
+                    <Typography textAlign= "center" variant ="h1">Events List</Typography>
+      
+                </Container>
             </Fragment>
             <Fragment>
                 {data.map((item) => (
